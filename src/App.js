4e260dc19bca03e79f3bc599d3a3e79b812aab29
Chernyshov_Nikita <xpom5566@mail.ru>
2020-03-04 19:37:02 +0300

    feat: test lego-react-json-view

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import { json } from "./testJson";
+
+import Viewer from "lego-react-json-view";
 
 function App() {
   return (
@@ -8,16 +12,19 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          DEMO: Lego-react-json-view{" "}
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            ( NPM package link )
+          </a>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        {/* <Viewer /> */}
+        <Viewer json={json} />
       </header>
     </div>
   );
---
+++ src/testJson.js
@@ -0,0 +1,17 @@
+export const json = {
+  hello: {
+    test: "json",
+    hide: {
+      this: {
+        path: {
+          test: "value1",
+          test2: "value2",
+          test3: "value3"
+        }
+      }
+    },
+    and: {
+      this: ["hello", "here", "my", "friend"]
+    }
+  }
+};
